3b8fb95e8767c1a2df0a998474cdf8988763b0e9
thzero <thzero@users.noreply.github.com>
2022-12-20 08:01:19 -0600

    updates for opensource

---
+++ openSource.js
@@ -0,0 +1,18 @@
+export default () => {
+    return [
+        {
+            category: 'server',
+            name: '@thzero/library_common',
+            url: 'https://github.com/thzero/library_common',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common/blob/master/license.md'
+        },
+        {
+            category: 'server',
+            name: '@thzero/library_common_service',
+            url: 'https://github.com/thzero/library_common_service',
+            licenseName: 'MIT',
+            licenseUrl: 'https://github.com/thzero/library_common_service/blob/master/license.md'
+        }
+    ];
+}
