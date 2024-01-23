6c9bd68a59f278d94a9c03099cf96dd7f6088003
thzero <thzero@users.noreply.github.com>
2024-01-22 21:32:45 -0600

    updates

--- constants.js
+++ constants.js
@@ -1,7 +1,6 @@
 const LibraryCommonServiceConstants = {
 	InjectorKeys: {
 		SERVICE_CONFIG: 'config',
-		SERVICE_HEALTH: 'serviceHealth',
 		SERVICE_LOGGER: 'serviceLogger',
 		SERVICE_MONITORING: 'serviceMonitoring',
 		SERVICE_VALIDATION: 'serviceValidation'
