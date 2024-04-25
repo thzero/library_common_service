11bb1f274e770c15a4cdb2439869cd35bf601d6b
thzero <thzero@users.noreply.github.com>
2024-04-25 08:44:28 -0500

    fix issue due to cross usage in client and server

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const LibraryCommonServiceConstants = {
 	InjectorKeys: {
-		SERVICE_CONFIG: 'config',
+		SERVICE_CONFIG: 'serviceConfig',
 		SERVICE_LOGGER: 'serviceLogger',
 		SERVICE_MONITORING: 'serviceMonitoring',
 		SERVICE_VALIDATION: 'serviceValidation'
